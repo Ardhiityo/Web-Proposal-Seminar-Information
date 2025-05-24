1caed9742915e789775f3ca0f5fcae37dbe9f7ba
belajar-vue-2 <aryaadi229@gmail.com>
2025-05-24 15:11:11 +0700

    refactor(views): improve table layout and column formatting in proposal export view
    The changes enhance the readability and structure of the proposal export view by adjusting table layouts, consolidating column spans, and improving alignment. This ensures a cleaner and more consistent presentation of data.

--- resources/views/exports/proposal/index.blade.php
+++ resources/views/exports/proposal/index.blade.php
@@ -1,34 +1,41 @@
 <table>
     <tr>
-        <th rowspan="3">
+        <th rowspan="4">
             <img src="{{ public_path('img/unival.jpeg') }}" width="100" height="100">
         </th>
-        <th style="font-size: 14px; font-weight: bold; text-align: left;" colspan="3">
+        <th style="font-size: 14px; font-weight: bold; text-align: left;" colspan="8">
             UNIVERSITAS AL-KHAIRIYAH
         </th>
     </tr>
     <tr>
         <td>
-            Website :
+            Website
         </td>
-        <td>
-            www.unival-cilegon.ac.id
+        <td colspan="4">
+            : www.unival-cilegon.ac.id
         </td>
     </tr>
     <tr>
         <td>
-            e-Mail :
-
+            e-Mail
         </td>
+        <td colspan="4">
+            : humas@unival-cilegon.ac.id
+        </td>
+    </tr>
+    <tr>
         <td>
-            humas@unival-cilegon.ac.id
+            Alamat
+        </td>
+        <td colspan="8">
+            : Jalan Kh. Ahmad Dahlan Kel. Citangkil Kota Cilegon
         </td>
     </tr>
 </table>
 
 <table>
     <tr>
-        <td colspan="22" style="border-top: 1px solid black;"></td>
+        <td colspan="22" style="border-top: 2px double black;"></td>
     </tr>
     <tr></tr>
     <tr></tr>
@@ -52,18 +59,23 @@
 <table>
     <thead>
         <tr>
-            <th style="font-weight: bold; border: 1px solid black; text-align: center; width: 40px;" colspan="1">No
-            </th>
-            <th style="font-weight: bold; border: 1px solid black; text-align: center;" colspan="1">NIM</th>
-            <th style="font-weight: bold; border: 1px solid black; text-align: center;" colspan="2">MAHASISWA</th>
-            <th style="font-weight: bold; border: 1px solid black; text-align: center;" colspan="1">TANGGAL</th>
-            <th style="font-weight: bold; border: 1px solid black; text-align: center;" colspan="1">WAKTU</th>
-            <th style="font-weight: bold; border: 1px solid black; text-align: center;" colspan="1">RUANGAN</th>
-            <th style="font-weight: bold; border: 1px solid black; text-align: center;" colspan="3">PEMBIMBING 1</th>
-            <th style="font-weight: bold; border: 1px solid black; text-align: center;" colspan="3">PEMBIMBING 2</th>
-            <th style="font-weight: bold; border: 1px solid black; text-align: center;" colspan="3">PENGUJI 1</th>
-            <th style="font-weight: bold; border: 1px solid black; text-align: center;" colspan="3">PENGUJI 2</th>
-            <th style="font-weight: bold; border: 1px solid black; text-align: center;" colspan="3">KETUA SIDANG</th>
+            <th style="font-weight: bold; border: 1px solid black; text-align: center;">No</th>
+            <th style="font-weight: bold; border: 1px solid black; text-align: center;">NIM</th>
+            <th style="font-weight: bold; border: 1px solid black; text-align: center;" colspan="2">
+                MAHASISWA</th>
+            <th style="font-weight: bold; border: 1px solid black; text-align: center;">TANGGAL</th>
+            <th style="font-weight: bold; border: 1px solid black; text-align: center;">WAKTU</th>
+            <th style="font-weight: bold; border: 1px solid black; text-align: center;">RUANGAN</th>
+            <th style="font-weight: bold; border: 1px solid black; text-align: center;" colspan="3">
+                PEMBIMBING 1</th>
+            <th style="font-weight: bold; border: 1px solid black; text-align: center;" colspan="3">
+                PEMBIMBING 2</th>
+            <th style="font-weight: bold; border: 1px solid black; text-align: center;" colspan="3">
+                PENGUJI 1</th>
+            <th style="font-weight: bold; border: 1px solid black; text-align: center;" colspan="3">
+                PENGUJI 2</th>
+            <th style="font-weight: bold; border: 1px solid black; text-align: center;" colspan="3">
+                KETUA SIDANG</th>
         </tr>
     </thead>
     <tbody>
@@ -83,29 +95,15 @@
                 <td style="border: 1px solid black; text-align: center; vertical-align: middle;">
                     {{ $proposal->room->name }}</td>
                 <td style="border: 1px solid black; text-align: center; vertical-align: middle;" colspan="3">
-                    {{ $proposal->student->lecture1->nidn }}
-                    -
-                    {{ $proposal->student->lecture1->name }}
-                </td>
+                    {{ $proposal->student->lecture1->nidn }} - {{ $proposal->student->lecture1->name }}</td>
                 <td style="border: 1px solid black; text-align: center; vertical-align: middle;" colspan="3">
-                    {{ $proposal->student->lecture2->nidn }}
-                    -
-                    {{ $proposal->student->lecture2->name }}
-                </td>
+                    {{ $proposal->student->lecture2->nidn }} - {{ $proposal->student->lecture2->name }}</td>
                 <td style="border: 1px solid black; text-align: center; vertical-align: middle;" colspan="3">
-                    {{ $proposal->examiner1->nidn }}
-                    -
-                    {{ $proposal->examiner1->name }}</td>
+                    {{ $proposal->examiner1->nidn }} - {{ $proposal->examiner1->name }}</td>
                 <td style="border: 1px solid black; text-align: center; vertical-align: middle;" colspan="3">
-                    {{ $proposal->examiner2->nidn }}
-                    -
-                    {{ $proposal->examiner2->name }}
-                </td>
+                    {{ $proposal->examiner2->nidn }} - {{ $proposal->examiner2->name }}</td>
                 <td style="border: 1px solid black; text-align: center; vertical-align: middle;" colspan="3">
-                    {{ $proposal->moderator->nidn }}
-                    -
-                    {{ $proposal->moderator->name }}
-                </td>
+                    {{ $proposal->moderator->nidn }} - {{ $proposal->moderator->name }}</td>
             </tr>
         @endforeach
     </tbody>
